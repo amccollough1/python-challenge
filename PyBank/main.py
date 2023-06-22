e61151d767d1d6be5c3f276b123463a7d9e11c07
amccollough1 <133404805+amccollough1@users.noreply.github.com>
2023-06-21 23:28:16 -0500

    Completed PyBank

--- PyBank/main.py
+++ PyBank/main.py
@@ -1,6 +1,6 @@
 import os
 import csv
-
+#print formatted data 
 print ("" " ")
 print("Financial Analysis")
 print("------------------------------")
@@ -16,21 +16,38 @@ with open (budgetcsv) as csvfile:
 
     #read through each row after header
     for row in csvreader:
+        #Add months of column A from data set 
         months.append(row[0])
+        #Add net total amount of column B from data set 
         net_total.append(int(row[1]))
+     #Assign variable total_months to the length of months add to the open list.    
     total_months=len(months) 
-    
+    #print format 
     print("Total Months: " + str(total_months))
+    #sum the net total of profit/losses
     print("Total: $" + str(sum(net_total)))
-
+    #Add average change
     avg_change= []
+    #calculate changes in average 
     for avg in range(1, len(net_total)):
         avg_change.append((int(net_total[avg])- int(net_total[avg-1])))
     rev_avg=round(sum(avg_change)/ len(avg_change),2)
 
     print("Average change: $ " + str(rev_avg))  
+    #calculate greatest increase/decrease 
+    greatest_inc = max(avg_change)
+    greatest_dec = min(avg_change)
+    print ("Greatest Increase in Profits: " + str(months[avg_change.index(max(avg_change))+1]) + " ($ " + str(greatest_inc)+ ")")
+    print ("Greatest Decrease in Profits: " + str(months[avg_change.index(min(avg_change))+1]) + " ($ " + str(greatest_dec)+ ")")
 
-    greatest_increase = max(avg_change)
-    greatest_decrease = min(avg_change)
-    print ("Greatest Increase in Profits: " + str(months[avg_change.index(max(avg_change))+1]) + " ($ " + str(greatest_increase)+ ")")
-    print ("Greatest Decrease in Profits: " + str(months[avg_change.index(min(avg_change))+1]) + " ($ " + str(greatest_decrease)+ ")")
+
+#write to txt file
+output= open('output.txt',  "w")
+output.write("Financial Analysis \n")
+output.write("--------------------\n")
+output.write("Total Months: " + str(total_months) + "\n")
+output.write("Total: $" + str(sum(net_total))+ "\n")
+output.write("Average change: $ " + str(rev_avg) +"\n")
+output.write("Greatest Increase in Profits: " + str(months[avg_change.index(max(avg_change))+1]) + " ($ " + str(greatest_inc)+ ") \n")
+output.write("Greatest Decrease in Profits: " + str(months[avg_change.index(min(avg_change))+1]) + " ($ " + str(greatest_dec)+ ") \n")
+output.close()
